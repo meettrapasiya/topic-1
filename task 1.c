5d1d85f8a99dcaffb0a7e89351d6f5b898011739
meettrapasiya <trapasiyameet78@gmail.com>
2024-06-20 12:12:49 +0530

    Add files via upload

---
+++ task 1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+main()
+
+{
+	printf(" name :- meet\n ");
+	printf(" fhather's name :- bharatbhai\n ");
+	printf(" surname :- trapasiya\n ");
+	printf(" age :- 19\n ");
+	printf(" school :- gayatri sansakar vidhyalay\n ");
+}
